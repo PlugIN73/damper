c632cadef2775a0994e7e320ac1f789472d74201
plugin73 <laplugin73@gmail.com>
2013-11-02 17:26:04 +0100

    self message send

--- lib/damper/forward_worker.rb
+++ lib/damper/forward_worker.rb
@@ -1,10 +1,16 @@
+require 'net/http'
+require 'uri'
 module Damper
   class ForwardWorker
     
     include Celluloid
 
     def perform(options)
-      puts self.inspect
+      data = JSON.parse(options)
+      #TODO use assync
+      url = URI.parse("http://127.0.0.1:3000")
+      request = Net::HTTP.new(url.host, url.port)
+      request.send_request(data["method"].upcase, data["request_uri"], data["body"], data["headers"])
     end
   end
 end
