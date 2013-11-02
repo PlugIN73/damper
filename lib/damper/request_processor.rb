9dd794d46b43bbc9c6277552b8077af38b128705
plugin73 <laplugin73@gmail.com>
2013-11-02 10:31:29 +0100

    redis test

--- lib/damper/request_processor.rb
+++ lib/damper/request_processor.rb
@@ -1,10 +1,18 @@
+require 'redis'
+
 module Damper
   class RequestProcessor
     def start
+      redis_client = Redis.new
       puts 'Starting reel web server'
       Reel::Server.run('127.0.0.1', 3000) do |connection|
         connection.each_request do |request|
+          redis_client.set('foo','bar')
           request.respond :ok, "hello, world!"
+
+          data = redis_client.get('foo')
+
+          puts data
         end
       end
     end
