809e7c9a623ae6a10841ea9c813fd2b95c13a1a0
plugin73 <laplugin73@gmail.com>
2013-11-08 19:11:54 +0100

    send request to forward_to option

--- lib/damper/forward_worker.rb
+++ lib/damper/forward_worker.rb
@@ -7,10 +7,10 @@ module Damper
 
     def perform(options)
       data = JSON.parse(options)
-      #TODO use assync
-      url = URI.parse("http://127.0.0.1:3000")
+      url = URI.parse(data["forward_to"])
       request = Net::HTTP.new(url.host, url.port)
-      request.send_request(data["method"].upcase, data["request_uri"], data["body"], data["headers"])
+      response = request.send_request(data["method"].upcase, data["request_uri"], data["body"], data["headers"])
+      puts response.inspect
     end
   end
 end
