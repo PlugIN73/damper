ccb4a8ef305914eed0ea652c8054cd80fe553184
plugin73 <laplugin73@gmail.com>
2014-06-01 09:11:20 +0000

    routing :D

--- lib/damper.rb
+++ lib/damper.rb
@@ -1,4 +1,5 @@
 require "damper/version"
+require "damper/supported_langs"
 require "damper/request_processor"
 require "damper/forward_manager"
 require "damper/forward_worker"
@@ -9,6 +10,6 @@ require "reel"
 require "json"
 
 module Damper
-  DEFAULT_HOST = "127.0.0.1"
+  DEFAULT_HOST = "0.0.0.0"
   DEFAULT_PORT = 3000
 end
--- lib/damper/request_processor.rb
+++ lib/damper/request_processor.rb
@@ -7,25 +7,40 @@ module Damper
       @namespace = options.delete(:namespace)
       @host = options.delete(:host) || Damper::DEFAULT_HOST
       @port = options.delete(:port) || Damper::DEFAULT_PORT
-      @callback_url = options.delete(:callback_url)
     end
 
     def start
-      client = Damper::Backend.redis
       describe_start
       Reel::Server.run(@host, @port) do |connection|
         connection.each_request do |request|
-          client.publish @namespace, prepare_data(request)
-          request.respond :ok, "message recieved"
+          serve request
         end
       end
+    end
+
+    def serve (request)
+      client = Damper::Backend.redis
+      if (request.path == "/")
+        if (CGI::parse(request.body.to_s)["callback_url"].first)
+          client.publish @namespace, prepare_data(request)
+          request.respond :ok, "message recieved"
+        else
+          request.respond :bad_request, "callback url not specified"
+        end
+      end
+
+      if (request.path == "/get_langs" || request.path == "/get_langs/")
+        request.respond :ok, Damper::SUPPORTED_LANGS.to_json
+      end
+      request.respond :not_found
+
     end
 
     def prepare_data(request)
       {
         code: CGI::parse(request.body.to_s)["code"].to_s,
+        callback_url: CGI::parse(request.body.to_s)["callback_url"].first.to_s,
         run: true,
-        callback_url: @callback_url
       }.to_json
     end
 
